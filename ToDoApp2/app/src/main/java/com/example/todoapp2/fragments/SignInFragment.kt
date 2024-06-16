603036285530772d90258a825afde97ccf589655
Ciobi0212 <ciobanuandrei0212@gmail.com>
2024-06-16 18:13:42 +0300

    Implement Auth

--- ToDoApp2/app/src/main/java/com/example/todoapp2/fragments/SignInFragment.kt
+++ ToDoApp2/app/src/main/java/com/example/todoapp2/fragments/SignInFragment.kt
@@ -5,15 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.todoapp2.R
+import com.example.todoapp2.databinding.FragmentSignInBinding
+import com.example.todoapp2.databinding.FragmentSignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SignInFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navControl: NavController
+    private lateinit var binding: FragmentSignInBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerEvents()
+    }
+
+    private fun init(view: View) {
+        navControl = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun registerEvents() {
+
+        binding.btnSignUpRedirect.setOnClickListener {
+            navControl.navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
+
+        binding.nextBtn.setOnClickListener {
+            val email = binding.emailEt.text.toString().trim()
+            val pass = binding.passEt.text.toString().trim()
+            if(email.isEmpty() || pass.isEmpty()) {
+                Toast.makeText(context, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+
+            }
+           else {
+                auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        Toast.makeText(context, "User logged in successfully", Toast.LENGTH_SHORT).show()
+                        navControl.navigate(R.id.action_signInFragment_to_homeFragment)
+                    }
+                    else {
+                        Toast.makeText(context, "Error: ${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
--- ToDoApp2/app/src/main/java/com/example/todoapp2/fragments/SplashFragment.kt
+++ ToDoApp2/app/src/main/java/com/example/todoapp2/fragments/SplashFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.todoapp2.R
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navControl: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -15,5 +21,29 @@ class SplashFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+
+        // show the splash screen for 4 seconds and then check if the user is logged in or not
+        view.postDelayed({
+            if(auth.currentUser != null) {
+                navControl.navigate(R.id.action_splashFragment_to_homeFragment)
+            }
+            else {
+                navControl.navigate(R.id.action_splashFragment_to_signInFragment)
+            }
+        }, 4000)
+    }
+
+    private fun init(view: View) {
+        navControl = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+
 
 }
